279d9eaae4a006b3d10de73f4beb3e25f55ea893
Denis Ilyushin <to@denisilyushin.ru>
2025-05-19 10:27:32 +0300

    upd tests

--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,4 @@ var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDc2MjkxOTcsInB3ZCI6InNlY3JldDEyMyJ9.iXrqnY92UA77lMD9OYF74WM1Dmk_4SYg2owFyYns-DU`
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDc2NjgzMDEsInB3ZCI6InNlY3JldDEyMzQifQ.75U_EvVF1UoQ1RXcAJHTn6DOlXKCy50gjbHNANvanfI`
